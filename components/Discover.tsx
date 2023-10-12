2af9421e921455acf8c61b74385cd632400c4700
wenhwang97 <wenhwang@bu.edu>
2023-10-12 18:32:35 -0400

    reconfigure bottom portion of homepage for mobile view

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,10 +1,14 @@
 const Discover = () => {
   return (
-    <div className=" flex justify-center my-16">
-      <div className="p-12 max-w-7xl grid grid-cols-4 gap-16">
+    <div className="flex justify-center my-16">
+      <div className="p-8 max-w-7xl md:grid md:gap-16 md:grid-cols-4 md:p-12">
         <div className="col-span-3 flex flex-col items-start justify-center">
-          <h5 className="text-5xl font-bold font-sans mb-6">Discover Mental Health Resources for Teens</h5>
-          <p className="text-lg font-sans mb-6">Explore our wide range of articles and resources for teens.</p>
+          <h5 className="text-4xl font-bold font-sans mb-4 md:mb-6 md:text-5xl">
+            Discover Mental Health Resources for Teens
+          </h5>
+          <p className="text:md font-sans mb-4 md:mb-6 md:text-lg">
+            Explore our wide range of articles and resources for teens.
+          </p>
         </div>
         <div>
           <button
