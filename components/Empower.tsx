fbe822dd3e9421b3fdff135d83ec6ee11a686718
wenhwang97 <wenhwang@bu.edu>
2023-10-12 17:06:42 -0400

    reconfigure empower section for mobile view

--- components/Empower.tsx
+++ components/Empower.tsx
@@ -10,11 +10,11 @@ interface EmpowerProps {
 const Empower = ({ title, description, image }: EmpowerProps) => {
   return (
     <div className="flex justify-center my-16">
-      <div className="p-12 max-w-7xl grid grid-cols-2 gap-16">
+      <div className="p-8 max-w-7xl grid gap-8 md:gap-16 md:grid-cols-2 md:p-12">
         <div className="flex flex-col items-start justify-center">
-          <span className="text-lg font-bold font-sans mb-4">Empower</span>
-          <h5 className="text-5xl font-bold font-sans mb-6">{title}</h5>
-          <p className="text-lg font-sans mb-6">{description}</p>
+          <span className="text-lg font-bold font-sans mb-2 md:mb-4">Empower</span>
+          <h5 className="text-4xl font-bold font-sans mb-4 md:mb-6 md:text-5xl">{title}</h5>
+          <p className="text-md font-sans mb-4 md:mb-6 md:text-lg">{description}</p>
           <button
             type="button"
             className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
