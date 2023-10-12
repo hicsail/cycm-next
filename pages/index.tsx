50e8644314064d1484638c4cd575a01567d13dbb
wenhwang97 <wenhwang@bu.edu>
2023-10-12 18:26:47 -0400

    reconfigure middle portion of homepage for mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-import { Inter, Nunito } from 'next/font/google'
-import Hero from '@/components/Hero';
-import Mission from '@/components/Mission';
-import Slider from '@/components/Slider';
-import Partners from '@/components/Partners';
-import Footer from '@/components/Footer';
-import CurrentState from '@/components/CurrentState';
-import Empower from '@/components/Empower';
-import ResourceCard from '@/components/ResourceCard';
-import Discover from '@/components/Discover';
+import Image from "next/image";
+import { Inter, Nunito } from "next/font/google";
+import Hero from "@/components/Hero";
+import Mission from "@/components/Mission";
+import Slider from "@/components/Slider";
+import Partners from "@/components/Partners";
+import Footer from "@/components/Footer";
+import CurrentState from "@/components/CurrentState";
+import Empower from "@/components/Empower";
+import ResourceCard from "@/components/ResourceCard";
+import Discover from "@/components/Discover";
 
-const inter = Inter({ subsets: ['latin'] });
-const nunito = Nunito({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 // flex min-h-screen flex-col items-center justify-between p-24
 const resources = [
@@ -38,36 +38,37 @@ const resources = [
 const sliderDataList = [
   {
     id: 1,
-    content: 'Generate age appropriate, culturally relevant mental health information',
-    image: <Image src="/cycm-slider-1.png" alt="CYCM Circle" width={1200} height={400} objectFit='cover' />
+    content: "Generate age appropriate, culturally relevant mental health information",
+    image: <Image src="/cycm-slider-1.png" alt="CYCM Circle" width={1200} height={400} objectFit="cover" />,
   },
   {
     id: 2,
-    content: 'Partner with trusted community organizations dedicated to supporting the wellbeing of youth to develop mental health focused programming and training',
-    image: <Image src="/cycm-slider-2.png" alt="CYCM Circle" width={1200} height={400} objectFit='cover'/>
+    content:
+      "Partner with trusted community organizations dedicated to supporting the wellbeing of youth to develop mental health focused programming and training",
+    image: <Image src="/cycm-slider-2.png" alt="CYCM Circle" width={1200} height={400} objectFit="cover" />,
   },
   {
     id: 3,
     content: `Engage in research initiatives that aim to develop evidence-based interventions specifically targeting the unique mental health needs of diverse youth
     `,
-    image: <Image src="/cycm-slider-3.png" alt="CYCM Circle" width={1200} height={400} objectFit='cover' />
-  }
+    image: <Image src="/cycm-slider-3.png" alt="CYCM Circle" width={1200} height={400} objectFit="cover" />,
+  },
 ];
 
-const partnersDataList : Array<any> = [
+const partnersDataList: Array<any> = [
   {
     id: 1,
-    image: <Image src="/img130.png" alt="CYCM Circle" width={200} height={200} objectFit='cover' />
+    image: <Image src="/img130.png" alt="CYCM Circle" width={200} height={200} objectFit="cover" />,
   },
   {
     id: 2,
-    image: <Image src="/we-belong.png" alt="CYCM Circle" width={200} height={200} objectFit='cover' />
+    image: <Image src="/we-belong.png" alt="CYCM Circle" width={200} height={200} objectFit="cover" />,
   },
   {
     id: 3,
-    image: <Image src="/bpc.png" alt="CYCM Circle" width={200} height={200} objectFit='cover' />
+    image: <Image src="/bpc.png" alt="CYCM Circle" width={200} height={200} objectFit="cover" />,
   },
-]
+];
 
 export default function Home() {
   return (
@@ -80,10 +81,10 @@ export default function Home() {
         image="/cycm-circle.jpg"
       />
       <div className="flex flex-col items-center my-16">
-        <h5 className="text-5xl font-bold font-sans mb-12 max-w-7xl px-12">
+        <h5 className="text-center font-bold font-sans mb-12 max-w-7xl px-8 text-4xl md:px-12 md:text-5xl">
           Discover Mental Health Resources for Teens
         </h5>
-        <div className="flex flex-row max-w-7xl gap-10 px-12">
+        <div className="flex max-w-7xl gap-10 px-8 flex-col md:px-12 md:flex-row">
           {resources.map((resource) => (
             <ResourceCard
               title={resource.title}
@@ -101,5 +102,5 @@ export default function Home() {
       {/* <Partners partners={partnersDataList} /> */}
       <Footer />
     </main>
-  )
+  );
 }
