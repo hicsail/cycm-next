9cc94f61b61d41e5906e807889fe9c3d0d03c4ef
wenhwang97 <wenhwang@bu.edu>
2023-10-27 17:00:53 -0400

    fix feed page spacing

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -31,8 +31,10 @@ export default function Feed() {
       </div>
       <div className="flex justify-center my-16">
         <div className="p-8 max-w-7xl md:p-12 w-full">
-          <span className="text-lg font-bold font-sans mb-4 md:mb-6">Blog</span>
-          <h5 className="font-bold font-sans mb-6 text-4xl md:text-4xl">Short Heading Gose Here</h5>
+          <div className="flex flex-col items-start justify-center">
+            <span className="text-lg font-bold font-sans mb-4 md:mb-6">Blog</span>
+            <h5 className="font-bold font-sans mb-6 text-4xl md:text-4xl">Short Heading Gose Here</h5>
+          </div>
           <p className="text-md font-sans mb-4 md:mb-6 md:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
             tristique.
